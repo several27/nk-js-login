11c3553d2b509fa67b25bd9e2a319a876c7711f2
Maciej Szpakowski <several27@icloud.com>
2014-07-18 19:20:11 +0200

    Create callback.php

---
+++ callback.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <script type="text/javascript">
+  
+  	nk = {};
+  	nk.options = {};
+  	nk.options.ssl_domain = 'nk.pl';
+  	nk.options.domain = 'nk.pl';
+  	nk.OAuth = {};
+  
+  	(function()
+  	{
+  		try
+  		{
+  			var oAuthWindow = null;
+  			if(window.opener && window.opener.isDestOAuthWindow)
+  				oAuthWindow = window.opener;
+  			else if(window.parent && window.parent.isDestOAuthWindow)
+  				oAuthWindow = window.parent;
+  
+  			if((/access_token=/.test(window.location.hash) || (/error=/.test(window.location.hash))) && oAuthWindow)
+  			{
+  				oAuthWindow.$windowScope.naszaKlasaCallback = window.location.href + "&dest_frame=true";
+  				oAuthWindow.$windowScope.$apply();
+  
+  				window.close();
+  			}
+  		}
+  		catch(b){}
+  	})();
+  
+  </script>
+</head>
+<body>
+</body>
+</html
